8ab941f6e3a824cab6d457fde090157acfb27c98
huangxinping <o0402@outlook.com>
2014-09-09 18:06:47 +0800

    增加提示性文件架构。

---
+++ App/Core/DataStruct/DSUserInfo.h
@@ -0,0 +1,13 @@
+//
+//  DSUserInfo.h
+//  App
+//
+//  Created by huangxinping on 9/9/14.
+//  Copyright (c) 2014 ShareMerge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSUserInfo : NSObject
+
+@end
---
+++ App/Core/DataStruct/DSUserInfo.m
@@ -0,0 +1,13 @@
+//
+//  DSUserInfo.m
+//  App
+//
+//  Created by huangxinping on 9/9/14.
+//  Copyright (c) 2014 ShareMerge. All rights reserved.
+//
+
+#import "DSUserInfo.h"
+
+@implementation DSUserInfo
+
+@end
